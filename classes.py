4c368018a7ce8bbd0fdebc7ab1d5493fdee829f9
AugustMusande <augustmusande@gmail.com>
2024-05-24 19:11:28 +0200

    ajouts des classes document dela quelle va deriver la classe livre ainsi que ajouts de la classepersonne de la quelle va deriver la classe membre ainsi qu'une modification dans la classe bibliotheque

--- classes.py
+++ classes.py
@@ -1,20 +1,42 @@
-class Livre:
-    def __init__(self, titre, auteur, isbn):
-        # Initialise un objet Livre avec un titre, un auteur et un ISBN
+#ajout de la classe document
+class Document:
+    def __init__(self, titre, auteur, id_document):
+        # Initialise un objet Document avec un titre, un auteur et un identifiant unique
         self.titre = titre
         self.auteur = auteur
+        self.id_document = id_document
+        self.disponible = True  # Par défaut, un document est disponible
+
+    def __str__(self):
+        # Retourne une chaîne de caractères décrivant le document
+        return f"{self.titre} par {self.auteur} (ID: {self.id_document}) - {'Disponible' if self.disponible else 'Emprunté'}"
+
+class Livre(Document):
+    def __init__(self, titre, auteur, isbn):
+        # Initialise un objet Livre en appelant le constructeur de Document
+        super().__init__(titre, auteur, isbn)
         self.isbn = isbn
-        self.disponible = True  # Par défaut, un livre est disponible
 
     def __str__(self):
         # Retourne une chaîne de caractères décrivant le livre
         return f"{self.titre} par {self.auteur} (ISBN: {self.isbn}) - {'Disponible' if self.disponible else 'Emprunté'}"
 
-class Membre:
-    def __init__(self, nom, id_membre):
-        # Initialise un objet Membre avec un nom et un identifiant unique
+
+
+class Personne:
+    def __init__(self, nom, id_personne):
+        # Initialise un objet Personne avec un nom et un identifiant unique
         self.nom = nom
-        self.id_membre = id_membre
+        self.id_personne = id_personne
+
+    def __str__(self):
+        # Retourne une chaîne de caractères décrivant la personne
+        return f"Personne: {self.nom} (ID: {self.id_personne})"
+
+class Membre(Personne):
+    def __init__(self, nom, id_membre):
+        # Initialise un objet Membre en appelant le constructeur de Personne
+        super().__init__(nom, id_membre)
         self.livres_empruntes = []  # Liste des livres empruntés par le membre
 
     def emprunter_livre(self, livre):
@@ -35,21 +57,22 @@ class Membre:
 
     def __str__(self):
         # Retourne une chaîne de caractères décrivant le membre et les livres empruntés
-        return f"Membre: {self.nom} (ID: {self.id_membre}) - Livres empruntés: {[livre.titre for livre in self.livres_empruntes]}"
+        return f"Membre: {self.nom} (ID: {self.id_personne}) - Livres empruntés: {[livre.titre for livre in self.livres_empruntes]}"
+
 
 class Bibliotheque:
     def __init__(self):
-        # Initialise une bibliothèque avec des listes vides pour les livres et les membres
-        self.livres = []
+        # Initialise une bibliothèque avec des listes vides pour les documents et les membres
+        self.documents = []
         self.membres = []
 
-    def ajouter_livre(self, livre):
-        # Ajoute un livre à la bibliothèque
-        self.livres.append(livre)
+    def ajouter_document(self, document):
+        # Ajoute un document à la bibliothèque
+        self.documents.append(document)
 
-    def supprimer_livre(self, isbn):
-        # Supprime un livre de la bibliothèque en utilisant son ISBN
-        self.livres = [livre for livre in self.livres if livre.isbn != isbn]
+    def supprimer_document(self, id_document):
+        # Supprime un document de la bibliothèque en utilisant son identifiant unique
+        self.documents = [doc for doc in self.documents if doc.id_document != id_document]
 
     def ajouter_membre(self, membre):
         # Ajoute un membre à la bibliothèque
@@ -57,33 +80,33 @@ class Bibliotheque:
 
     def supprimer_membre(self, id_membre):
         # Supprime un membre de la bibliothèque en utilisant son identifiant unique
-        self.membres = [membre for membre in self.membres if membre.id_membre != id_membre]
+        self.membres = [membre for membre in self.membres if membre.id_personne != id_membre]
 
-    def emprunter_livre(self, id_membre, isbn):
-        # Permet à un membre d'emprunter un livre s'il est disponible
+    def emprunter_document(self, id_membre, id_document):
+        # Permet à un membre d'emprunter un document s'il est disponible
         # Trouve le membre par son identifiant
-        membre = next((m for m in self.membres if m.id_membre == id_membre), None)
-        # Trouve le livre par son ISBN
-        livre = next((l for l in self.livres if l.isbn == isbn), None)
-        # Si le membre et le livre existent, tente l'emprunt
-        if membre and livre:
-            return membre.emprunter_livre(livre)
-        return False  # Retourne False si le membre ou le livre n'existe pas
+        membre = next((m for m in self.membres if m.id_personne == id_membre), None)
+        # Trouve le document par son identifiant
+        document = next((d for d in self.documents if d.id_document == id_document), None)
+        # Si le membre et le document existent, tente l'emprunt
+        if membre and document:
+            return membre.emprunter_livre(document)
+        return False  # Retourne False si le membre ou le document n'existe pas
 
-    def retourner_livre(self, id_membre, isbn):
-        # Permet à un membre de retourner un livre emprunté
+    def retourner_document(self, id_membre, id_document):
+        # Permet à un membre de retourner un document emprunté
         # Trouve le membre par son identifiant
-        membre = next((m for m in self.membres if m.id_membre == id_membre), None)
-        # Trouve le livre par son ISBN
-        livre = next((l for l in self.livres if l.isbn == isbn), None)
-        # Si le membre et le livre existent, tente le retour
-        if membre and livre:
-            return membre.retourner_livre(livre)
-        return False  # Retourne False si le membre ou le livre n'existe pas
+        membre = next((m for m in self.membres if m.id_personne == id_membre), None)
+        # Trouve le document par son identifiant
+        document = next((d for d in self.documents if d.id_document == id_document), None)
+        # Si le membre et le document existent, tente le retour
+        if membre and document:
+            return membre.retourner_livre(document)
+        return False  # Retourne False si le membre ou le document n'existe pas
 
-    def rechercher_livre(self, titre):
-        # Recherche des livres par titre (insensible à la casse)
-        return [livre for livre in self.livres if titre.lower() in livre.titre.lower()]
+    def rechercher_document(self, titre):
+        # Recherche des documents par titre (insensible à la casse)
+        return [doc for doc in self.documents if titre.lower() in doc.titre.lower()]
 
     def rechercher_membre(self, nom):
         # Recherche des membres par nom (insensible à la casse)
@@ -93,13 +116,13 @@ class Bibliotheque:
 bibliotheque = Bibliotheque()
 
 # Création de livres et d'un membre
-livre1 = Livre("1984", "George bouche", "360")
-livre2 = Livre("Le Meilleur des Mondes", "Exaudie", "360")
-membre1 = Membre("August", 1)
+livre1 = Livre("2004", "exowan", "360")
+livre2 = Livre("Le Meilleur des Mondes", "ingid", "987")
+membre1 = Membre("gentine", 1)
 
 # Ajout des livres et du membre à la bibliothèque
-bibliotheque.ajouter_livre(livre1)
-bibliotheque.ajouter_livre(livre2)
+bibliotheque.ajouter_document(livre1)
+bibliotheque.ajouter_document(livre2)
 bibliotheque.ajouter_membre(membre1)
 
 # Affichage d'un livre et d'un membre
@@ -107,11 +130,12 @@ print(livre1)  # Affiche les détails du livre 1
 print(membre1)  # Affiche les détails du membre 1
 
 # Emprunt d'un livre par le membre
-bibliotheque.emprunter_livre(1, "360")
+bibliotheque.emprunter_document(1, "360")
 print(livre1)  # Le livre 1 est maintenant marqué comme emprunté
 print(membre1)  # Le membre 1 a maintenant le livre 1 dans sa liste de livres empruntés
 
 # Retour d'un livre par le membre
-bibliotheque.retourner_livre(1, "369")
+bibliotheque.retourner_document(1, "987")
 print(livre1)  # Le livre 1 est maintenant marqué comme disponible
 print(membre1)  # Le membre 1 n'a plus de livres empruntés
+
